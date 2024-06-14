7dcc20245bfb6ac5588cc850cec5934d996f618f
Johan Johnsson <johan.johnsson@outlook.com>
2024-06-14 18:23:37 +0200

    Fixed issue that caused adding feats to not work after a app reload

--- Arcemi.Models/Blueprints/GameBlueprintsArchive.cs
+++ Arcemi.Models/Blueprints/GameBlueprintsArchive.cs
@@ -10,8 +10,8 @@ namespace Arcemi.Models
     public class GameBlueprintsArchive : IDisposable
     {
         private readonly FileInfo _blueprintsFile;
-        private readonly FileStream _blueprintsStream;
-        private readonly ZipArchive _blueprints;
+        private FileStream _blueprintsStream;
+        private ZipArchive _blueprints;
         private readonly IGameResourcesProvider _res;
 
         public GameBlueprintsArchive(string gameFolder, IGameResourcesProvider res)
@@ -19,15 +19,15 @@ namespace Arcemi.Models
             if (string.IsNullOrEmpty(gameFolder)) return;
             _blueprintsFile = new FileInfo(Path.Combine(gameFolder, "Blueprints.zip"));
             if (!_blueprintsFile.Exists) return;
-
-            _blueprintsStream = new FileStream(_blueprintsFile.FullName, FileMode.Open, FileAccess.Read, FileShare.Read);
-            _blueprints = new ZipArchive(_blueprintsStream);
             _res = res;
         }
 
         public Blueprint Load(IBlueprintMetadataEntry metadata)
         {
-            if (_blueprints is null) return null;
+            if (_blueprints is null) {
+                _blueprintsStream = new FileStream(_blueprintsFile.FullName, FileMode.Open, FileAccess.Read, FileShare.Read);
+                _blueprints = new ZipArchive(_blueprintsStream);
+            }
             var actualPath = metadata.Path.StartsWith("Blueprints\\", StringComparison.OrdinalIgnoreCase)
                 ? metadata.Path.Remove(0, 11)
                 : metadata.Path;
@@ -48,8 +48,15 @@ namespace Arcemi.Models
 
         public void Dispose()
         {
-            if (_blueprints is object) _blueprints.Dispose();
-            if (_blueprintsStream is object) _blueprintsStream.Dispose();
+            if (_blueprints is object) {
+                _blueprints.Dispose();
+                _blueprints = null;
+            }
+
+            if (_blueprintsStream is object) {
+                _blueprintsStream.Dispose();
+                _blueprintsStream = null;
+            }
         }
     }
 }
